63d7c780bb0289c917236c976b0535764640b134
Connor Dinnadge <36665474+connordinnadge@users.noreply.github.com>
2024-06-26 20:07:38 +0100

    switched crop type to select field

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -3,14 +3,58 @@ import type { NextComponentType, NextPageContext } from 'next'
 import { Input } from '@/components/ui/input'
 import { useEffect, useState } from 'react'
 import { Separator } from '../ui/separator'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
+
+const cropFactors: { value: string; label: string }[] = [
+    {
+        label: 'Full-Frame (35mm)',
+        value: '1.0',
+    },
+    {
+        label: 'APS-C (Nikon, Sony, Pentax)',
+        value: '1.5',
+    },
+    {
+        label: 'APS-C (Canon)',
+        value: '1.6',
+    },
+    {
+        label: 'Micro Four Thirds (MFT)',
+        value: '2.0',
+    },
+    {
+        label: 'Medium Format',
+        value: 'varies, commonly less than 1.0',
+    },
+    {
+        label: '1-inch',
+        value: '2.7',
+    },
+    {
+        label: '1.5-inch',
+        value: '2.0',
+    },
+    {
+        label: '1/2.3-inch',
+        value: '5.6',
+    },
+    {
+        label: '1/1.7-inch',
+        value: '4.5',
+    },
+    {
+        label: '1/3.2-inch',
+        value: '7.6',
+    },
+]
 
 const Index: NextComponentType<NextPageContext> = () => {
     const [focalLength, setFocalLength] = useState(12)
-    const [cropFactor, setCropFactor] = useState(1.6)
-    const [exposureTime, setExposureTime] = useState(Math.floor(500 / (focalLength ?? 1 * cropFactor ?? 1)))
+    const [cropFactor, setCropFactor] = useState('')
+    const [exposureTime, setExposureTime] = useState(Math.floor(500 / (focalLength ?? 1 * parseFloat(cropFactor) ?? 1)))
 
     useEffect(() => {
-        if (focalLength >= 1 && cropFactor >= 0.1) setExposureTime(Math.floor(500 / (focalLength * cropFactor)))
+        if (focalLength >= 1 && cropFactor) setExposureTime(Math.floor(500 / (focalLength * parseFloat(cropFactor))))
     }, [focalLength, cropFactor])
 
     return (
@@ -28,14 +72,18 @@ const Index: NextComponentType<NextPageContext> = () => {
             </div>
             <div className='my-2'>
                 <p className='mb-2 font-semibold'>Crop Factor</p>
-                <Input
-                    className='bg-black bg-opacity-60'
-                    type='number'
-                    value={cropFactor}
-                    onChange={(e) => setCropFactor(parseFloat(e.target.value))}
-                    min={0.1}
-                    max={10}
-                />
+                <Select value={cropFactor} onValueChange={setCropFactor}>
+                    <SelectTrigger className='bg-black bg-opacity-60'>
+                        <SelectValue placeholder='Select crop factor' />
+                    </SelectTrigger>
+                    <SelectContent className='bg-black bg-opacity-85 text-white'>
+                        {cropFactors.map((item) => (
+                            <SelectItem key={item.value} value={item.value}>
+                                {item.label}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                </Select>
             </div>
             <Separator className='my-5' />
             <p className='line text-2xl font-semibold uppercase'>Exposure Time</p>
